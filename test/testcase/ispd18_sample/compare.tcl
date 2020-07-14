c615be4f488dce09a6f62488e8eabe1842192951
Matt Liberty <matt.liberty@gmail.com>
2020-07-14 15:24:47 -0700

    create golden file for unit test and actually check it

--- test/testcase/ispd18_sample/compare.tcl
+++ test/testcase/ispd18_sample/compare.tcl
@@ -11,19 +11,36 @@ set test_drc [format "%12d" 0]
 set golden_drc [format "%12d" 0]
 
 if {[file exists $golden_log]} {
-  catch {set golden_wirelength [format "%12d" [exec grep -e {total wire length = } $golden_log | tail -1 awk {{print $5}}]]}
-  catch {set golden_via [format "%12d" [exec grep -e {total number of vias = } $golden_log | tail -1 awk {{print $6}}]]}
-  catch {set golden_drc [format "%12d" [exec grep -e {number of violations = } $golden_log | tail -1 awk {{print $5}}]]}
+  set golden_wirelength [format "%12d" [exec grep -e {total wire length = } $golden_log | tail -1 | awk {{print $5}}]]
+  set golden_via [format "%12d" [exec grep -e {total number of vias = } $golden_log | tail -1 | awk {{print $6}}]]
+  set golden_drc [format "%12d" [exec grep -e {number of violations = } $golden_log | tail -1 | awk {{print $5}}]]
+} else {
+  puts "golden file $golden_log not found"
+  exit 1
 }
 
 if {[file exists $test_log]} {
-  catch {set test_wirelength [format "%12d" [exec grep -e {total wire length = } $test_log | tail -1 awk {{print $5}}]]}
-  catch {set test_via [format "%12d" [exec grep -e {total number of vias = } $test_log | tail -1 awk {{print $6}}]]}
-  catch {set test_drc [format "%12d" [exec grep -e {number of violations = } $test_log | tail -1 awk {{print $5}}]]}
-}
-
-if { $golden_wirelength >= $test_wirelength && $golden_via >= $test_via && $golden_drc >= $test_drc } {
-  exit 0
+  set test_wirelength [format "%12d" [exec grep -e {total wire length = } $test_log | tail -1 | awk {{print $5}}]]
+  set test_via [format "%12d" [exec grep -e {total number of vias = } $test_log | tail -1 | awk {{print $6}}]]
+  set test_drc [format "%12d" [exec grep -e {number of violations = } $test_log | tail -1 | awk {{print $5}}]]
 } else {
+  puts "test file $test_log not found"
   exit 1
 }
+
+if { $golden_wirelength < $test_wirelength } {
+  puts "Wire Length: golden=$golden_wirelength test=$test_wirelength"
+  exit 1
+}
+
+if { $golden_via < $test_via } {
+  puts "Vias: golden=$golden_via test=$test_via"
+  exit 1
+}
+
+if { $golden_drc < $test_drc } {
+  puts "DRC: golden=$golden_drc test=$test_drc"
+  exit 1
+}
+
+exit 0
