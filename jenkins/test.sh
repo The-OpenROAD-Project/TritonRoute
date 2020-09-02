026cd1a27cfa6105112fdd448dba08521efa4ea5
Matt Liberty <matt.liberty@gmail.com>
2020-09-02 11:44:16 -0700

    run cpp unittests in Jenkins

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/tritonroute tritonroute bash -c "cd /tritonroute/test/ && ./unit_test.sh /tritonroute/build/TritonRoute"
+docker run -v $(pwd):/tritonroute tritonroute bash -c "make -C /tritonroute/build test && cd /tritonroute/test/ && ./unit_test.sh /tritonroute/build/TritonRoute"
